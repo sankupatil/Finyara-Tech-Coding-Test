b20cb7f90ac1f785bf1198022e34ed4e1430c0eb
sankupatil <100894878+sankupatil@users.noreply.github.com>
2022-11-19 21:29:33 +0530

    Add files via upload

---
+++ SamplePuzzle5/src/main/java/com/example/demo/SamplePuzzle5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePuzzle5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePuzzle5Application.class, args);
+	}
+
+}
